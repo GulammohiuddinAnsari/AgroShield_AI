5024f4387fd91653e0eed432619c92b30289d953
Gulammohiuddinansari <gulammohiuddin0712@gmail.com>
2026-09-23 00:52:15 +0530

    debug banner makes false to hide from page

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class AgroShieldApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'AgroShield AI',
       theme: ThemeData(
         primarySwatch: Colors.green,
